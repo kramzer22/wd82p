a52709125a7fd2895cdeb3519461d856009e975d
kramzer22 <markdavidpanganiban1218@hotmail.com>
2023-09-09 12:43:08 +0800

    add start and end game

--- 23-rock-paper-scissors/script.js
+++ 23-rock-paper-scissors/script.js
@@ -1,3 +1,7 @@
+const startGameButton = document.getElementById("start-game");
+const startGameMessage = document.getElementById("popup-display");
+const startGameContainer = document.getElementById("popup-container");
+
 const moveRock = document.getElementById("move-rock");
 const movePaper = document.getElementById("move-paper");
 const moveScissors = document.getElementById("move-scissors");
@@ -25,6 +29,33 @@ moveScissors.addEventListener("click", function () {
   mainGame(2);
 });
 
+const startGame = () => {
+  playerScoreValue = 0;
+  tieScoreValue = 0;
+  computerScoreValue = 0;
+
+  playerScore.textContent = "0";
+  tieScore.textContent = "0";
+  computerScore.textContent = "0";
+
+  playerTurn.textContent = "#";
+  computerTurn.textContent = "#";
+
+  startGameContainer.style.opacity = "0";
+  setTimeout(() => setPopupToHidden(), 400);
+  console.log("game start");
+};
+
+function setPopupToHidden() {
+  startGameContainer.style.display = "none";
+}
+
+function setPopupToBlock() {
+  startGameContainer.style.opacity = "1";
+}
+
+startGameButton.addEventListener("click", startGame);
+
 const getComputerMove = () => {
   let computerMove = Math.round(Math.random() * (moveList.length - 1));
   console.log("Computer move: " + moveList[computerMove]);
@@ -40,6 +71,12 @@ const mainGame = (playerMove) => {
   result = challengePlayerMove(playerMove, computerMove);
 
   displayResult(result, playerMove, computerMove);
+
+  if (playerScoreValue === 10) {
+    endGame(true);
+  } else if (computerScoreValue === 10) {
+    endGame(false);
+  }
 };
 
 const challengePlayerMove = (playerMove, computerMove) => {
@@ -76,4 +113,19 @@ const displayResult = (result, playerMove, computerMove) => {
 
   playerTurn.textContent = moveList[playerMove];
   computerTurn.textContent = moveList[computerMove];
+};
+
+const endGame = (result) => {
+  if (result === true) {
+    startGameMessage.textContent = "YOU WIN!";
+  } else {
+    startGameMessage.textContent = "YOU LOSE!";
+  }
+
+  startGameButton.textContent = "PLAY AGAIN";
+
+  startGameContainer.style.display = "block";
+  setTimeout(() => setPopupToBlock(), 400);
+
+  console.log("game start");
 };
